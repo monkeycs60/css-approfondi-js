e533afcb43812ed84654654c9db68a58dc14ca4f
monkeycs60 <clement.serizay@gmail.com>
2022-03-12 15:22:26 +0100

    bouton js

--- scriptjs.js
+++ scriptjs.js
@@ -1,21 +1,11 @@
-// 3. Créez une fonction qui sert à repérer si un texte qu'on lui passe en argument contient la lettre "z".
-// si c'est le cas, la fonction écrit dans la console "Alerte: le texte contient la lettre Z".
 
-function reveleZ(text) {
-     
-     for (const zapeZ of text) {
 
-          switch (zapeZ) {
-               case "z":
-                    console.log("Alerte: le texte contient la lettre Z");
-                    break;
-          
-               default:
-                    console.log("Le texte ne contient pas la lettre Z");
-                    break;
-                    
-          }
 
-     }
+
+const boutonPage = document.querySelector('input');
+boutonPage.addEventListener("click", changeCouleur);
+
+
+function changeCouleur() {
+     boutonPage.style.background = "blue";
 }
-     reveleZ("je suis une  bonne soeur ")
