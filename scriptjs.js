49750b4f18f3ee0e5b0d73b203820c89caea5ba9
monkeycs60 <clement.serizay@gmail.com>
2022-03-13 22:27:51 +0100

    fonction ordre sup

--- scriptjs.js
+++ scriptjs.js
@@ -1,6 +1,15 @@
-const tab = [1,2,3];
 
-for (const iterator of tab) {
-     console.log(iterator);
+
+     const valeurs = [1, 2, 3,4,5,6,7,8,9];
+     function filtre(arr, fonctionne) {
+          const filtred = [];
+          for (const elements of arr) {
+               if(fonctionne(elements)) {
+                    filtred.push(elements);
+                    
+               }
+          }
           
      }
+     const filtredvalue = filtre(valeurs, plus => plus > 3);
+console.log(filtredvalue);
