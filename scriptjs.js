3723dcaf1608c6100751b0b6446102e85a26a870
monkeycs60 <clement.serizay@gmail.com>
2022-03-14 01:34:33 +0100

    fonction pure

--- scriptjs.js
+++ scriptjs.js
@@ -1,15 +1,40 @@
+// EXERCICE 8 : Les fonctions 🔥
 
 
-     const valeurs = [1, 2, 3,4,5,6,7,8,9];
-     function filtre(arr, fonctionne) {
-          const filtred = [];
-          for (const elements of arr) {
-               if(fonctionne(elements)) {
-                    filtred.push(elements);
-                    
-               }
-          }
-          
+
+
+// // 1. Écrire une fonction pure qui sert à multiplier deux nombres ensemble.
+
+// function multiplyByTwo(a, b) {
+//      return a*b;
+//      console.log(a*b);
+     
+// }
+
+// console.log(multiplyByTwo(2,8));
+
+
+
+
+
+// 2. Écrire une fonction d'ordre supérieur, qui prend une fonction callback en paramètre et un tableau rempli de nombres.
+//   Cette fonction retournera un nouveau tableau, qui contient toutes les valeurs auxquelles on a ajouté 1
+
+
+
+function AddOne(newtableauNombres, PlusUn) {
+     const tableauNombres = [];
+for (let index = 0; index < array.length; index++) {
+         tableauNombres.push(PlusUn(newtableauNombres[index]));
+        
      }
-     const filtredvalue = filtre(valeurs, plus => plus > 3);
-console.log(filtredvalue);
+     
+     return tableauNombres;
+     
+}
+
+const PlusUn = nb => nb + 1;
+   
+   const NouveauTruc =  AddOne([1,2,3,4,5,11], PlusUn);
+  
+
